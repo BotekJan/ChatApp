fdd6397ac54c1bdb39e0df9514c1fef80f0bd8f6
Your Name <youremail@yourdomain.com>
2021-11-22 12:40:43 +0100

    CORS

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ mongoose.connect('mongodb+srv://brozovic_michal:'
     useMongoClient: true
 })
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
 app.use('/auth', authRoutes);
 
 module.exports = app;
