17cce23eae8faf440527585e07048b54d808b578
Your Name <youremail@yourdomain.com>
2021-11-22 13:06:29 +0100

    app.js

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-var cors = require('cors');
+const authRoutes = require("./api/routes/auth");
+const chatRoutes = require("./api/routes/chat");
 
+mongoose.connect(
+  "mongodb+srv://botek:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-rest-shop.k0mor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+mongoose.Promise = global.Promise;
 
-const authRoutes = require('./api/routes/auth');
-const chatRoutes = require('./api/routes/chat');
-
-const mongoose = require('mongoose');
-
-
-
-
-mongoose.connect('mongodb+srv://brozovic_michal:' 
-+ process.env.MONGO_ATLAS_PW + 
-'@chatapp.wqbas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-{
-    useMongoClient: true
-})
-
-app.use(cors());
-
-app.use('/auth', authRoutes);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use("/auth", authRoutes);
+app.use("/chat", chatRoutes);
+
+//Error handeling
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
   });
-
-
+});
 
 module.exports = app;
