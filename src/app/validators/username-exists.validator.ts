23fae4c73b3011b87e05e2c45aab3393ebd6787a
BotekJan <72377065+BotekJan@users.noreply.github.com>
2021-11-27 11:38:31 +0100

    validator

---
+++ src/app/validators/username-exists.validator.ts
@@ -0,0 +1,9 @@
+// import { map } from 'rxjs/operators';
+// import { AbstractControl, AsyncValidatorFn } from "@angular/forms";
+// import { AuthService } from "../welcome/auth.service";
+
+// export function couseTitleValidator(auth: AuthService):AsyncValidatorFn {
+//     return (control: AbstractControl) => {
+//         return auth.usernameExists(control.value).usernameExists
+//     }
+// }
