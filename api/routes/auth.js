341862f74fa53ea0ffd320a923fb3b04b25d0d80
BotekJan <72377065+BotekJan@users.noreply.github.com>
2021-11-29 12:39:18 +0100

    fix?

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -27,25 +27,25 @@ router.post("/usernameExists", (req, res, next) => {
         });
 });
 
-router.post("/register", (req, res, next) => {
-    const novyUzivatel = new Uzivatel({
-        _id: mongoose.Types.ObjectId(),
-        jmeno: req.body.jmeno,
-        pratele: [],
-        obrazek: 'default',
-        nastaveni: {}
-    })
-    novyUzivatel.save().then(result => {
-        res.status(201).json({
-            message: 'User succesfully created',
-            user: result,
-        })
-    }).catch((err) => {
-        res.status(500).json({ error: err });
+// router.post("/register", (req, res, next) => {
+//     const novyUzivatel = new Uzivatel({
+//         _id: mongoose.Types.ObjectId(),
+//         jmeno: req.body.jmeno,
+//         pratele: [],
+//         obrazek: 'default',
+//         nastaveni: {}
+//     })
+//     novyUzivatel.save().then(result => {
+//         res.status(201).json({
+//             message: 'User succesfully created',
+//             user: result,
+//         })
+//     }).catch((err) => {
+//         res.status(500).json({ error: err });
 
 
-    });
-})
+//     });
+// })
 
     router.post("/login", (req, res, next) => {
         res.status(200).json({
