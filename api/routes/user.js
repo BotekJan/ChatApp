97a193b1c95df89dcac48c622c22b57ca9c75469
introper <michalhasic03@seznam.cz>
2021-12-13 13:15:38 +0100

    auth

---
+++ api/routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const checkAuth = require("../middleware/check-auth")
+
+router.post("/", checkAuth,(req, res, next) => {
+    Uzivatel.findOne({ jmeno: req.body.jmeno })
+      .then((user) => {
+        if (user) {
+          return res.status(200).json({
+            usernameExists: user
+          });
+        }
+        return res.status(200).json({
+          message: "User does not exist",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+  });
