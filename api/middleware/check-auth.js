a6ef6023c285eafa4a1afe16140f0d7cde035ab1
BotekJan <72377065+BotekJan@users.noreply.github.com>
2021-12-16 08:47:29 +0100

    fix

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.eports = (req, res, next) => {
+module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
