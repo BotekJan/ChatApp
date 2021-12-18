356eb115fecbd1bfc7c38150292f60df031efe99
BotekJan <72377065+BotekJan@users.noreply.github.com>
2021-12-18 17:05:42 +0100

    debug test addition

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,12 +2,16 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     if(!req.headers.Authorization){
-        return res.status(401).send('Unauthorized request')
+        return res.status(401).json({
+            message: 'no request header Authorization'
+        });
     }
     try {
         const token = req.headers.Authorization.split(" ")[1];
         if(token === 'null'){
-            return res.status(401).send('Unauthorized request')
+            return res.status(401).json({
+                message: 'request header Authorization is null'
+            });
         }
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         if(!payload){
