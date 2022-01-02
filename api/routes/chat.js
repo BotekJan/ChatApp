3fd4a3815d11221828e2d98b85e756394b038696
BotekJan <72377065+BotekJan@users.noreply.github.com>
2022-01-02 15:56:49 +0100

    again

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 const checkAuth = require("../middleware/check-auth");
 const Chat = require("../models/chat");
